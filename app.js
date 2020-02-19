39888cd165ff61bed1795d73dad6fac291a75ce5
tmurvv <tmurv@shaw.ca>
2020-02-19 12:11:54 -0700

    initial commit able to serve JSON harp list

---
+++ app.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const express = require('express');
+//const Holidays = require('date-holidays');
+
+// const morgan = require('morgan');
+// const rateLimit = require('express-rate-limit');
+// const helmet = require('helmet');
+
+// const mongoSanitize = require('express-mongo-sanitize');
+
+// const xss = require('xss-clean');
+// const hpp = require('hpp');
+// const compression = require('compression');
+// const cookieParser = require('cookie-parser');
+
+// const globalErrorHandler = require('./controllers/errorController');
+// const AppError = require('./utils/AppError');
+// const bookingController = require('./controllers/bookingController');
+// const bookingItemController = require('./controllers/bookingItemController');
+// const globalErrorHandler = require('./controllers/errorController');
+// const rentalRoomRouter = require('./routes/rentalRoomRoutes');
+// const userRouter = require('./routes/userRoutes');
+// const bookingRouter = require('./routes/bookingRoutes');
+// const bookingItemRouter = require('./routes/bookingItemRoutes');
+const viewRouter = require('./routes/viewRoutes');
+// const companyProfileRouter = require('./routes/companyProfileRoutes');
+
+const app = express();
+//app.set('view engine', 'pug');
+//app.set('views', path.join(__dirname, 'views'));
+console.log(process.env.NODE_ENV);
+
+//const hd = new Holidays('CA', 'ab');
+
+//security
+// app.use(helmet()); //variety of security protections
+// const limiter = rateLimit({
+//     max: 200,
+//     windowMs: 60 * 60 * 1000,
+//     message: 'Too many requests from this IP, please try again in an hour.'
+// });
+// app.use('/api', limiter); //limits calls from each api (brute force attack)
+// app.use(express.json({ limit: '10kb' }));
+// app.use(cookieParser());
+// app.use(mongoSanitize()); //must come after express.json statement, eliminate special chars
+// app.use(xss()); //further data sanitization
+// app.use(hpp()); //cleans up url parameters, you can whitelist params you want more than one search on
+
+//utilities
+app.use(express.static(path.join(__dirname, 'public')));
+// app.use(compression());
+
+// app.get('/addBooking', (req, res) => {
+//     res.status(200).render('post');
+// });
+
+// *************
+//  * ROUTES
+//  ************/
+
+//Rental room routes & middleware
+app.use('/', viewRouter);
+// app.use('/api/v1/rentalrooms', rentalRoomRouter);
+// app.use('/api/v1/users', userRouter);
+// app.use('/api/v1/bookings', bookingRouter);
+// app.use('/api/v1/bookingItems', bookingItemRouter);
+// app.use('/api/v1/companyProfile', companyProfileRouter);
+
+//REFACTOR
+// app.post('/submitBooking', bookingController.createBooking);
+// app.post('/submitBookingItems', bookingItemController.createBookingItem);
+
+// app.all('*', (req, res, next) => {
+//     next(new AppError(`Page not found (${req.originalUrl}).`, 404));
+// });
+
+//uncomment when error handler put in place
+// app.use(globalErrorHandler);
+
+module.exports = app;
---
+++ pl-scraper.js
@@ -0,0 +1,45 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+const usedHarpsNorthAmerica = [];
+const url = 'https://www.harpconnection.com/harpstore/harp-UsedHarps.html';
+
+//Tisha Murvihill Harp Services
+usedHarpsNorthAmerica.push({
+    seller: "Tisha Murvihill",
+    sellerCountry: "Canada",
+    harpTitle: 'Sierra 36 by Triplett',
+    harpShortDesc: 'Purchased 2011 Maple',
+    harpPrice: '$4600',
+    harpLongDesc: 'Excellent condition, lightly used, beautiful Triplett sound. This one is a winner!',
+    harpPhotoUrl: 'photo goes here'
+})
+
+// Harp Connection
+axios(url)
+    .then(response => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const usedHarpTable = $('.plusplus');
+        usedHarpTable.each(function () {
+            const seller = 'Harp Connection';
+            const sellerCountry = 'USA';
+            const harpTitle = $(this).find('h3').text();
+            const harpShortDesc = $(this).parent().parent().find('p').first().text();
+            const harpPrice = $(this).parent().parent().find('.THCsmall').text();
+            const harpLongDesc = $(this).parent().parent().find('p:nth-child(2)').text();
+            
+            usedHarpsNorthAmerica.push({
+                seller,
+                sellerCountry,
+                harpTitle,
+                harpShortDesc,
+                harpPrice,
+                harpLongDesc
+            });
+        });
+        
+    })
+    .catch(console.error);
+
+    module.exports = usedHarpsNorthAmerica;
