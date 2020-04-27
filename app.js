67adf4f329a2c7be2501222974ffd445f5c3e65d
tmurvv <tmurv@shaw.ca>
2020-04-27 10:43:13 -0600

    all user endpoints working

--- app.js
+++ app.js
@@ -191,10 +191,97 @@ app.post('/api/v1/users/createuser', async (req, res) => {
         });
     } catch (e) {
         res.status(500).json({
-            title: 'FindAHarp.com | Create Contact',
+            title: 'FindAHarp.com | Create User',
             status: 'fail',
             data: {
                 message: `Something went wrong while creating user: ${e.message}`
+            }
+        });
+    }
+});
+app.get('/api/v1/users/', async (req, res) => {
+    try {
+        const allUsers = await Users.find();
+       
+        res.status(200).json({
+            title: 'FindAHarp.com | Get All Users',
+            status: 'success',
+            data: {
+                allUsers
+            }
+        });
+    } catch (e) {
+        res.status(500).json({
+            title: 'FindAHarp.com | Get All Users',
+            status: 'fail',
+            data: {
+                message: `Something went wrong getting all users: ${e.message}`
+            }
+        });
+    }
+});
+app.get('/api/v1/users/me/:userid', async (req, res) => {
+    console.log(req.params)
+    try {
+        const user = await Users.findById(req.params.userid);
+       
+        res.status(200).json({
+            title: 'FindAHarp.com | Get User',
+            status: 'success',
+            data: {
+                user
+            }
+        });
+    } catch (e) {
+        res.status(500).json({
+            title: 'FindAHarp.com | Get User',
+            status: 'fail',
+            data: {
+                message: `Something went wrong with your account: ${e.message}`
+            }
+        });
+    }
+});
+app.patch('/api/v1/users/updateuser/:userid', async (req, res) => {
+    try {
+        const user = await Users.findByIdAndUpdate(req.params.userid, req.body);
+       
+        res.status(200).json({
+            title: 'FindAHarp.com | Update User',
+            status: 'success',
+            data: {
+                message: 'User updated',
+                user
+            }
+        });
+    } catch (e) {
+        res.status(500).json({
+            title: 'FindAHarp.com | Update User',
+            status: 'fail',
+            data: {
+                message: `Something went wrong while updating user: ${e.message}`
+            }
+        });
+    }
+});
+app.delete('/api/v1/users/deleteuser/:userid', async (req, res) => {
+    try {
+        const user = await Users.findByIdAndDelete(req.params.userid);
+       
+        res.status(200).json({
+            title: 'FindAHarp.com | Delete User',
+            status: 'success',
+            data: {
+                message: 'User deleted',
+                user
+            }
+        });
+    } catch (e) {
+        res.status(500).json({
+            title: 'FindAHarp.com | Delete User',
+            status: 'fail',
+            data: {
+                message: `Something went wrong while deleting user: ${e.message}`
             }
         });
     }
@@ -210,96 +297,3 @@ app.use(globalErrorHandler);
 
 //Export
 module.exports = app;
-
-/***********
-*OLD CODE
-************/
-
-// const path = require('path');
-// const cors = require('cors');
-// const express = require('express');
-// //const Holidays = require('date-holidays');
-
-// // const morgan = require('morgan');
-// // const rateLimit = require('express-rate-limit');
-// // const helmet = require('helmet');
-
-// // const mongoSanitize = require('express-mongo-sanitize');
-
-// // const xss = require('xss-clean');
-// // const hpp = require('hpp');
-// // const compression = require('compression');
-// // const cookieParser = require('cookie-parser');
-
-// // const globalErrorHandler = require('./controllers/errorController');
-// // const AppError = require('./utils/AppError');
-// // const bookingController = require('./controllers/bookingController');
-// // const bookingItemController = require('./controllers/bookingItemController');
-// // const globalErrorHandler = require('./controllers/errorController');
-// // const rentalRoomRouter = require('./routes/rentalRoomRoutes');
-// // const userRouter = require('./routes/userRoutes');
-// // const bookingRouter = require('./routes/bookingRoutes');
-// // const bookingItemRouter = require('./routes/bookingItemRoutes');
-// const viewRouter = require('./routes/viewRoutes');
-// // const companyProfileRouter = require('./routes/companyProfileRoutes');
-// const app = express();
-// app.use(cors());
-// express.static('img');
-// app.use(express.static('img'));
-// //app.set('view engine', 'pug');
-// //app.set('views', path.join(__dirname, 'views'));
-// console.log(process.env.NODE_ENV);
-
-// //const hd = new Holidays('CA', 'ab');
-
-// //security
-// // app.use(helmet()); //variety of security protections
-// // const limiter = rateLimit({
-// //     max: 200,
-// //     windowMs: 60 * 60 * 1000,
-// //     message: 'Too many requests from this IP, please try again in an hour.'
-// // });
-// // app.use('/api', limiter); //limits calls from each api (brute force attack)
-// // app.use(express.json({ limit: '10kb' }));
-// // app.use(cookieParser());
-// // app.use(mongoSanitize()); //must come after express.json statement, eliminate special chars
-// // app.use(xss()); //further data sanitization
-// // app.use(hpp()); //cleans up url parameters, you can whitelist params you want more than one search on
-
-// //utilities
-// app.use(express.static(path.join(__dirname, 'public')));
-// // app.use(compression());
-
-// // app.get('/addBooking', (req, res) => {
-// //     res.status(200).render('post');
-// // });
-
-// // *************
-// //  * ROUTES
-// //  ************/
-
-// //Rental room routes & middleware
-// app.all('/', function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     // res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//     next()
-// });
-// app.use('/', viewRouter);
-// // app.use('/api/v1/rentalrooms', rentalRoomRouter);
-// // app.use('/api/v1/users', userRouter);
-// // app.use('/api/v1/bookings', bookingRouter);
-// // app.use('/api/v1/bookingItems', bookingItemRouter);
-// // app.use('/api/v1/companyProfile', companyProfileRouter);
-
-// //REFACTOR
-// // app.post('/submitBooking', bookingController.createBooking);
-// // app.post('/submitBookingItems', bookingItemController.createBookingItem);
-
-// // app.all('*', (req, res, next) => {
-// //     next(new AppError(`Page not found (${req.originalUrl}).`, 404));
-// // });
-
-// //uncomment when error handler put in place
-// // app.use(globalErrorHandler);
-
-// module.exports = app;
