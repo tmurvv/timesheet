3c7b363d29453ee8069865e6db5845708092bd46
tmurvv <tmurv@shaw.ca>
2020-07-02 12:27:56 -0600

    add email unique

--- assets/data/Schemas.js
+++ assets/data/Schemas.js
@@ -56,6 +56,7 @@ const usersSchema = new mongoose.Schema({
         type: String,
         trim: true,
         lowercase: true,
+        unique: true,
         required: 'Email address is required',
         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Email address not valid. Please try again.']
     },
