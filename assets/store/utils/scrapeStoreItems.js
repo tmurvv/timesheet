8498db54752a32866899ae67bc9d8b45bc8108ee
tmurvv <tmurv@shaw.ca>
2020-11-02 12:18:03 -0700

    one page get store items working

--- assets/store/utils/scrapeStoreItems.js
+++ assets/store/utils/scrapeStoreItems.js
@@ -3,12 +3,9 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const uuid = require('uuid');
 
-exports.scrapeStoreItems = async (seller, data) => {
-    console.log('imin scrape')
+async function getLinkInfo(url) {
     const response = await axios({url: `https://www.harpsetc.com/bow-brand-natural-gut-1st-octave-set-00g-f.html`, 'strictSSL': false});
     const html = response.data.text;
-    // console.log(html);
-
     const $ = cheerio.load(html);
 
     const product = {};
@@ -17,8 +14,58 @@ exports.scrapeStoreItems = async (seller, data) => {
     product.price = $('.ty-price-num').text().trim();
     product.description = $('.content-description').text().trim();
     product.image = $('.ty-pict   ').first().attr('src');
+    console.log(product)
+    return product;
+}
 
-    console.log('answer', product)
+exports.scrapeStoreItems = async (seller, data) => {
+    const response = await axios({url: `https://www.harpsetc.com/strings/bow-brand/bow-brand-natural-gut/`, 'strictSSL': false});
+    const html = response.data.text;
+    const $ = cheerio.load(html);
+    let productTable = $('.ty-product-list').toArray();
+    
+    const answer = await Promise.all(productTable.map(async item => {
+        const product = {};
+        const secondaryUrl = $(item).find('.ty-product-list__image').find('a').attr('href');
+        try {
+            const response = await axios({url: `${secondaryUrl}`, 'strictSSL': false});
+            const html = response.data.text;
+            // console.log(response.data.text)
+            const $S = cheerio.load(html);
+            
+            product.storeId = $S('.ty-product-block__left').find('form').attr('name').substring(13);
+            product.title = $S('.ty-product-block-title').text().trim();
+            product.price = $S('.ty-price-num').text().trim();
+            product.description = $S('.content-description').text().trim();
+            product.image = $S('.ty-pict   ').first().attr('src');
+            
+        } catch(e) {
+            console.log('error', e.message)
+        }         
+        return product;
+    }));
+    return answer;
+}
+
+
+
+//-------------------
+    // console.log('imin scrape')
+    // const response = await axios({url: `https://www.harpsetc.com/bow-brand-natural-gut-1st-octave-set-00g-f.html`, 'strictSSL': false});
+    // const html = response.data.text;
+    // // console.log(html);
+
+    // const $ = cheerio.load(html);
+
+    // const product = {};
+    // product.storeId = $('.ty-product-block__left').find('form').attr('name').substring(13);
+    // product.title = $('.ty-product-block-title').text().trim();
+    // product.price = $('.ty-price-num').text().trim();
+    // product.description = $('.content-description').text().trim();
+    // product.image = $('.ty-pict   ').first().attr('src');
+
+    // console.log('answer', product)
+    //-----------
     // const productTable = $('.ty-price-block');
     // console.log(($, item) => $(item).find('.ty-product-list__item-name').text().trim()); //titleFn
     // ($, item) => $(item).find('.ty-product-list__price').text().trim(), //priceFn 
@@ -30,5 +77,3 @@ exports.scrapeStoreItems = async (seller, data) => {
     // const productTable = $(seller.mainPathId);
     // console.log('Seller', seller.name)
     // console.log('# of Products:', productTable.length);
-    return {item: 'myItem'}
-}
