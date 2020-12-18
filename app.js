d50ee892e8e4ec9944f1f0cdb913fc3f7cf92d22
tmurvv <tmurv@shaw.ca>
2020-12-18 08:06:37 -0700

    deploy store agreements

--- app.js
+++ app.js
@@ -312,9 +312,95 @@ app.post('/api/v1/partners/westcoastharpsagree', async (req,res) => {
         });
     }
 });
+// Vixen Harps
+app.get('/api/v1/partners/vixenharps', (req,res) => {
+    res.status(200).render('base', {
+        seller: 'Vixen Harps',
+        sellerId: 'vixenharps',
+        startDate: 'December 18, 2020',
+        fee: '3%',
+        minimum: '$45.00cad'
+    });
+});
+app.post('/api/v1/partners/vixenharpsagree', async (req,res) => {
+    if (!(req.body.feecheck&&req.body.feecheck==='on'&&req.body.termscheck&&req.body.termscheck==='on')) {
+        return res.status(400).render('base', {
+            agreement: 'fail',
+            seller: 'Vixen Harps',
+            sellerId: 'vixenharps',
+            startDate: 'January 2, 2021',
+            fee: '3%',
+            minimum: '$45.00cad'
+        });
+    }
+    try {
+        const uploadagreement = Object.assign({ 
+            seller: req.body.seller,
+            sellerId: req.body.sellerId,
+            startdate: req.body.startdate,
+            fee: req.body.fee,
+            minimum: req.body.minimum,
+            scheduletext: req.body.scheduletext
+        });
+        const addedagreement = await Agreements.create(uploadagreement);
+        agreementSigned();
+        return res.status(200).render('base', {
+            agreement: 'success',
+            seller: req.body.seller
+        });
+    } catch (e) {
+        return res.status(500).render('base', {
+            error: 'server error',
+            message: e.message
+        });
+    }
+});
+// Blevins Harps
+app.get('/api/v1/partners/blevinsharps', (req,res) => {
+    res.status(200).render('base', {
+        seller: 'Blevins Harps',
+        sellerId: 'blevinsharps',
+        startDate: 'December 18, 2020',
+        fee: '5%',
+        minimum: '$40.00usd'
+    });
+});
+app.post('/api/v1/partners/blevinsharpsagree', async (req,res) => {
+    if (!(req.body.feecheck&&req.body.feecheck==='on'&&req.body.termscheck&&req.body.termscheck==='on')) {
+        return res.status(400).render('base', {
+            agreement: 'fail',
+            seller: 'Blevins Harps',
+            sellerId: 'blevinsharps',
+            startDate: 'December 18, 2020',
+            fee: '5%',
+            minimum: '$40.00usd'
+        });
+    }
+    try {
+        const uploadagreement = Object.assign({ 
+            seller: req.body.seller,
+            sellerId: req.body.sellerId,
+            startdate: req.body.startdate,
+            fee: req.body.fee,
+            minimum: req.body.minimum,
+            scheduletext: req.body.scheduletext
+        });
+        const addedagreement = await Agreements.create(uploadagreement);
+        agreementSigned();
+        return res.status(200).render('base', {
+            agreement: 'success',
+            seller: req.body.seller
+        });
+    } catch (e) {
+        return res.status(500).render('base', {
+            error: 'server error',
+            message: e.message
+        });
+    }
+});
 // Germaine
 app.get('/api/v1/partners/gl', (req,res) => {
-    res.status(200).render('base', {
+    res.status(200).render('germaine', {
         seller: 'Germaine Luyben',
         sellerId: 'germaine',
         startDate: 'January 7, 2021',
@@ -322,15 +408,15 @@ app.get('/api/v1/partners/gl', (req,res) => {
         minimum: '0'
     });
 });
-app.post('/api/v1/partners/westcoastharpsagree', async (req,res) => {
+app.post('/api/v1/partners/glagree', async (req,res) => {
     if (!(req.body.feecheck&&req.body.feecheck==='on'&&req.body.termscheck&&req.body.termscheck==='on')) {
-        return res.status(400).render('base', {
+        return res.status(400).render('germaine', {
             agreement: 'fail',
-            seller: 'West Coast Harps',
-            sellerId: 'westcoastharps',
-            startDate: 'January 7, 2021',
-            fee: '3%',
-            minimum: '$45.00cad'
+            seller: 'Germain Luyben',
+            sellerId: 'gl',
+            startDate: 'December 18, 2020',
+            fee: '25%',
+            minimum: ''
         });
     }
     try {
