85c2ef591d431b3cc636a20f2e778d070fc6e9a4
tmurvv <tmurv@shaw.ca>
2021-02-09 12:54:23 -0700

    deploy

--- app.js
+++ app.js
@@ -274,7 +274,7 @@ app.post('/api/v1/partners/vixenharpsagree', async (req,res) => {
 
 //#endregion
 // Willow Karlene
-app.get('/api/v1/partners/wk', (req,res) => {
+app.get('/api/v1/partners/wkagree', (req,res) => {
     res.status(200).render('base', {
         seller: 'Willow Karlene',
         sellerId: 'wk',
